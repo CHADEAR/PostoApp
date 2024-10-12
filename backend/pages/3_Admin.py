6685706af75f67143aa760573939aae35bcefdca
Chadear <93990946+CHADEAR@users.noreply.github.com>
2024-10-12 16:35:42 +0700

    Update 3_Admin.py

--- backend/pages/3_Admin.py
+++ backend/pages/3_Admin.py
@@ -3,6 +3,42 @@ import os
 import pandas as pd
 from model import read_name_from_image, crop_and_read_names, save_to_csv, count_names_in_csv
 
+def change_colors():
+    style = """
+        <style>
+            #upload-parcel-image {
+                color: #333366;  /* เปลี่ยนสีของคำว่า Login */
+            }
+            .st-emotion-cache-bm2z3a {
+                background-color: #f0f0f0;  /* สีพื้นหลัง */
+            }
+            .st-emotion-cache-h4xjwg{
+                background-color: #ff5f5f;  /* สีheader */
+            }
+            st-emotion-cache-1dp5vir{
+                background-color: #ff5f5f;  /* header */
+            }
+            .stText {
+                color: #333366;  /* สีของตัวอักษร */
+            }
+            .st-emotion-cache-1erivf3{
+                background-color: #333366;  /* สี upload */
+            }
+            st-emotion-cache-15hul6a{
+                background-color: #333366;  /* สี upload */
+            }
+            .stButton>button {
+                background-color: #f9e75e;  /*  */
+                color: #333366;  /* สีของตัวอักษรในปุ่ม */
+            }
+            .stButton>button:hover {
+                background-color: #f9e75e;  /* สีของปุ่มเมื่อชี้เมาส์ */
+            }
+        
+        </style>
+    """
+    st.markdown(style, unsafe_allow_html=True)
+
 def check_login():
     if "login_status" not in st.session_state or not st.session_state.login_status:
         st.warning("กรุณา Login ก่อนเข้าหน้าอื่น")
@@ -12,6 +48,16 @@ def check_login():
     if "email" not in st.session_state or st.session_state.email != "admin@adminbydorm.com":  # เปลี่ยนเป็นบัญชีที่อนุญาต
         st.warning("คุณไม่มีสิทธิ์เข้าถึงหน้านี้")
         st.stop()  # หยุดการทำงานถ้าผู้ใช้ไม่มีสิทธิ์
+
+def logout():
+    if "login_status" in st.session_state:
+        st.session_state.login_status = False
+    if "email" in st.session_state:
+        st.session_state.email = None  # ลบอีเมลออกจาก session
+    st.success("Logout สำเร็จ!")
+    st.session_state.current_page = "login"  # เปลี่ยนไปที่หน้า Login
+    st.switch_page("pages/1_Login.py")  # สลับไปยังหน้า Home
+    st.experimental_rerun()  # รีเฟรชหน้า
 
 # สร้างโฟลเดอร์สำหรับอัปโหลดถ้าไม่มี
 UPLOAD_FOLDER = 'uploads'
@@ -25,9 +71,10 @@ user_avatar = "https://firebasestorage.googleapis.com/v0/b/posto-ai-app.appspot.
 bot_avatar = "https://firebasestorage.googleapis.com/v0/b/posto-ai-app.appspot.com/o/robot.png?alt=media&token=99e37f4c-dbef-4d07-86a5-75e70585ac54"    # URL ของรูปโปรไฟล์ Chatbot
 
 def Admin():
+    change_colors()
     check_login()  # ตรวจสอบการล็อกอิน
-    st.title("หน้าอัปโหลด")
-    uploaded_file = st.file_uploader("อัปโหลดไฟล์", type=['jpg', 'png'])
+    st.title("Upload Parcel Image")
+    uploaded_file = st.file_uploader("Select Files", type=['jpg', 'png'])
         
     if uploaded_file is not None:
         image_path = os.path.join(UPLOAD_FOLDER, uploaded_file.name)
@@ -49,6 +96,8 @@ def Admin():
             st.write("Name Counts: ", count_names_in_csv().to_dict(orient='records'))
         else:
             st.warning("ไม่พบชื่อในภาพ")
+    if st.button("Logout"):
+        logout()  # เรียกฟังก์ชัน logout
 
 if __name__ == "__main__":
     Admin()
