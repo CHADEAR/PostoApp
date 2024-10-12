0f7204a5b96d081c43789bd2d8661a6c68895894
Chadear <93990946+CHADEAR@users.noreply.github.com>
2024-10-12 16:16:09 +0700

    Update 4_Chatbot.py

--- backend/pages/4_Chatbot.py
+++ backend/pages/4_Chatbot.py
@@ -19,8 +19,8 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 CSV_FILE = 'backend/names.csv'
 
-user_avatar = "/pages/user.png"
-bot_avatar = "/pages/robot.png"
+user_avatar = "user.png"
+bot_avatar = "robot.png"
 
 def chat():
     check_login()
