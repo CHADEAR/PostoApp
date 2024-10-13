d63da3c47ced48a4f671a7a47b8bf58d514bef26
Chadear <93990946+CHADEAR@users.noreply.github.com>
2024-10-13 10:47:45 +0700

    Update 4_Chatbot.py

--- backend/pages/4_Chatbot.py
+++ backend/pages/4_Chatbot.py
@@ -82,7 +82,7 @@ def chat():
         .st-emotion-cache-1vt4y43 {
             position: fixed;
             top: 0;
-            left :20;
+            left:80%;
             margin-top:7px;
             width: 80px; /* ขนาดที่เล็กลง */
             height: 50px; /* ขนาดที่เล็กลง */
