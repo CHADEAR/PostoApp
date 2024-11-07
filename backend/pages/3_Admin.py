b34b3f7acf8632f9f32599b52137223353afe055
Chadear <93990946+CHADEAR@users.noreply.github.com>
2024-11-07 14:40:16 +0700

    Update 3_Admin.py

--- backend/pages/3_Admin.py
+++ backend/pages/3_Admin.py
@@ -41,6 +41,12 @@ def change_colors():
             .st-emotion-cache-1vt4y43{
                 background-color: #C4D7FF;
             }
+            .st-emotion-cache-1igbibe {
+                position: fixed;
+                top: 10px;
+                right: 10px;
+                z-index: 1000;
+            }
         </style>
     """
     st.markdown(style, unsafe_allow_html=True)
