b6faffaa53ba9088e437222c8aaf17b754eaedcc
Chadear <93990946+CHADEAR@users.noreply.github.com>
2024-11-07 14:54:31 +0700

    Update 3_Admin.py

--- backend/pages/3_Admin.py
+++ backend/pages/3_Admin.py
@@ -37,7 +37,7 @@ def change_colors():
             }
             .logout-button {
                 position: fixed;
-                top: 50px;
+                top: 70px;
                 right: 10px;
                 background-color: #f9e75e;
                 color: #333366;
